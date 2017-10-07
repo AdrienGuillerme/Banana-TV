4277b4365ecb02e1783af918881ec3279e14408a
Arnaud MONTEILS <arnaud.monteils@isen.yncrea.fr>
2017-10-07 10:34:20 -0400

    Lava trap

---
+++ Assets/LavaSprite.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LavaSprite : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+            SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
