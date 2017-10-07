885289850266871610f71c5e3398fcb6f9bbaeba
Arnaud MONTEILS <arnaud.monteils@isen.yncrea.fr>
2017-10-06 23:32:18 -0400

    v0.0.2 add settings page  with volume control

---
+++ Assets/SoundVolumeControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundVolumeControl : MonoBehaviour
+{
+
+    public static double MusicSoundLevel = 0.5;
+    public static double FxSoundLevel = 0.5;
+    public static double AnchorManSoundLevel = 0.5;
+
+    public Slider MusicSoundSlider;
+    public Slider FxSoundSlider;
+    public Slider AnchorManSoundSlider;
+
+
+    public void SetMusicSoundLevel()
+    {
+        MusicSoundLevel = MusicSoundSlider.value;
+    }
+    public void SetFxSoundLevel()
+    {
+        FxSoundLevel = FxSoundSlider.value;
+    }
+    public void SetAnchorManSoundLevel()
+    {
+        AnchorManSoundLevel = AnchorManSoundSlider.value;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/StartingPageController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingPageController : MonoBehaviour {
+
+    public GameObject SettingsMenuGameObject; // Assign in inspector
+    public GameObject StartMenuGameObject; // Assign in inspector
+    private bool _settingsIsShown = false;
+
+
+
+    // Use this for initialization
+    void Start () { 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LaunchSettings()
+    {
+        _settingsIsShown = true;
+        SettingsMenuGameObject.SetActive(_settingsIsShown);
+        StartMenuGameObject.SetActive(!_settingsIsShown);
+
+    }
+
+    public void QuitSettings()
+    {
+        _settingsIsShown = false;
+        SettingsMenuGameObject.SetActive(_settingsIsShown);
+        StartMenuGameObject.SetActive(!_settingsIsShown);
+
+    }
+}
