c510e745a06980200fb908ee17b94daabe13dc04
Arnaud MONTEILS <arnaud.monteils@isen.yncrea.fr>
2017-10-07 16:32:00 -0400

    merge

---
+++ Assets/FallinRocksTrapScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallinRocksTrapScript : MonoBehaviour {
+
+    public GameObject DieMenu;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            DieMenu.SetActive(true);    
+            Time.timeScale = 0;
+        }
+
+        // Use this for initialization
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
