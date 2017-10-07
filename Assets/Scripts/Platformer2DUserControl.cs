e1bc0095abadf54678e1bd0547a10e80f3a74cfa
Barbuseries <francois.edorh@isen.yncrea.fr>
2017-10-07 18:53:46 +0200

    Bugfix no collision detected when player interacts with an element

--- Assets/Scripts/Platformer2DUserControl.cs
+++ Assets/Scripts/Platformer2DUserControl.cs
@@ -6,10 +6,14 @@ using UnityStandardAssets._2D;
 [RequireComponent(typeof (PlatformerCharacter2D))]
 public class Platformer2DUserControl : MonoBehaviour
 {
+	[SerializeField] private float m_InteractionDelay = 0.25f; // Minimum delay between two interactions
+		
 	public PlatformerCharacter2D m_Character;
 	private bool m_Jump;
 	private bool m_Interacting;
 	private bool m_CanMove = true;
+	private float m_TimeUntilNextInteraction = 0f;
+	
 
 	private void Awake()
 	{
@@ -23,7 +27,6 @@ public class Platformer2DUserControl : MonoBehaviour
 		{
 			// Read the jump input in Update so button presses aren't missed.
 			m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-			m_Interacting = CrossPlatformInputManager.GetButtonDown("Submit");
 		}
 	}
 
@@ -40,6 +43,19 @@ public class Platformer2DUserControl : MonoBehaviour
 		// Pass all parameters to the character control script.
 		m_Character.Move(h, crouch, m_Jump);
 		m_Jump = false;
+
+
+		if (m_TimeUntilNextInteraction > 0) {
+			m_TimeUntilNextInteraction -= Time.deltaTime;
+		}
+
+		if (m_TimeUntilNextInteraction < 0) {
+			m_TimeUntilNextInteraction = 0f;
+		}
+		
+		if (!m_Interacting && (m_TimeUntilNextInteraction == 0)) {
+			m_Interacting = CrossPlatformInputManager.GetButtonDown("Submit");
+		}
 	}
 
 	public void OnTriggerStay2D(Collider2D collider) {
@@ -51,6 +67,8 @@ public class Platformer2DUserControl : MonoBehaviour
 
 			// Only allow interacting with one element at a time.
 			m_Interacting = false;
+
+			m_TimeUntilNextInteraction = m_InteractionDelay;
 		}
 	}
 		
