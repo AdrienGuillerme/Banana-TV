ee99cc91de29971ddc544f9a50c11d2645b51537
AdrienGuillerme <adrien.guillerme@isen.yncrea.fr>
2017-10-07 23:48:20 -0400

    init intro scene

--- Assets/StartingPageController.cs
+++ Assets/StartingPageController.cs
@@ -22,9 +22,9 @@ public class StartingPageController : MonoBehaviour {
 		
 	}
 
-    public void LoadMainScene()
+    public void LoadIntroScene()
     {
-        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        SceneManager.LoadScene("Intro", LoadSceneMode.Single);
 
     }
     public void LaunchSettings()
