b2f944c0eb5c998ff7c08b7098cdfd62d26ba6b6
Alv0iD <dakoukou@hotmail.fr>
2017-10-08 19:19:05 +0200

    bloop

---
+++ Assets/Scripts/BalanceBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceBehaviourScript : MonoBehaviour {
+
+    public GameObject balance;
+    public double yMax;
+    public double yMin;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        balance.transform.position = new Vector2(balance.transform.position.x, (float)(yMin + (yMax - transform.position.y)));
+	}
+}
---
+++ Assets/Scripts/BouttonBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouttonBehaviourScript : MonoBehaviour
+{
+
+    public Renderer button;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            button.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            button.enabled = true;
+        }
+    }
+}
